3e16ef9ed1f490dddb57f8508a4989d413ff0f74
Yannck Morn <yannick.morin@mail.mcgill.ca>
2020-02-26 18:07:35 -0500

    :sparkles: spring boot init

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.eventregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationApplication.class, args);
+	}
+
+}
